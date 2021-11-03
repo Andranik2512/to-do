3a877d95b4355061236fc319cdf4919f92710317
Andranik <andranikaparan@gmail.com>
2021-11-03 15:37:22 +0200

    Add getItem

--- src/store/todoSlice.js
+++ src/store/todoSlice.js
@@ -8,6 +8,7 @@ const todoSlice = createSlice({
     reducers: {
         addTodo(state, action) {
             state.todos.push({
+                userid:sessionStorage.getItem('currentloggedin'),
                 id:new Date().toISOString(),
                 text: action.payload.text,
                 completed:false,
@@ -15,7 +16,7 @@ const todoSlice = createSlice({
               localStorage.setItem('todos',JSON.stringify([...state.todos]))
         },
         removeTodo(state, action) {
-            state.todos = state.todos.filter(todo => todo.id!==action.payload.id)
+            state.todos = state.todos.filter((todo) => todo.id!==action.payload.id)
             localStorage.setItem('todos', JSON.stringify([...state.todos]));
         },
         toggleTodoComplete(state, action) {
