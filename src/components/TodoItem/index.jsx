2955163c0e8e00e62f98b24c51186de7401d58f9
Andranik <andranikaparan@gmail.com>
2021-11-15 11:30:53 +0200

    Add edit todo and new style

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,35 +1,74 @@
-import React from 'react'
-import {useDispatch} from 'react-redux';
-import {removeTodo, toggleTodoComplete} from '../../store/todoSlice'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeTodo, toggleTodoComplete, editTodo } from '../../store/todoSlice';
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-import DeleteForeverTwoToneIcon from '@mui/icons-material/DeleteForeverTwoTone';
-import ModeEditOutlineTwoToneIcon from '@mui/icons-material/ModeEditOutlineTwoTone';
 
-const TodoItem = ({userid, id, text, completed, }) => {
-    const dispatch = useDispatch();
-    if(userid === sessionStorage.getItem('currentloggedin')){
-        
-    
-console.log(completed);
+const TodoItem = ({ userid, id, text, completed }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+  const idBtn = open ? 'simple-popover' : undefined;
+
+  const editTask = () => {
+    dispatch(editTodo({ text, id }));
+    handleClose();
+  };
+  function handleInput(e) {
+    text = e.target.value;
+  }
+  const dispatch = useDispatch();
+
+  if (userid === sessionStorage.getItem('currentloggedin')) {
+    console.log(completed);
     return (
-        <li>
-            <input 
-           type='checkbox' 
-           checked={completed}
-           onChange={() => dispatch(toggleTodoComplete({id}))}
-           />
-           <span>{text}</span>
-           <span className='delete' onClick={()=> dispatch(removeTodo({id}))}>
-               <buttom>
-               <DeleteForeverTwoToneIcon /> 
-               </buttom>
-               <buttom>
-               <ModeEditOutlineTwoToneIcon color="action"/> 
-               </buttom>
-           </span>   
-        </li>
-    )
-}
-return null;
-}
-export default TodoItem
+      <li style={{ margin: '10px' }}>
+        <input type="checkbox" checked={completed} onChange={() => dispatch(toggleTodoComplete({ id }))} />
+        <span>{text}</span>
+
+        <Button variant="outlined" size="small" onClick={() => dispatch(removeTodo({ id }))} startIcon={<DeleteIcon />}>
+          Delete
+        </Button>
+        <Button aria-describedby={idBtn} variant="outlined" size="small" onClick={handleClick}>
+          Edit Todo
+        </Button>
+        <Popover
+          id={idBtn}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+        >
+          <Typography sx={{ p: 2 }}>
+            <label>
+              <TextField
+                id="filled-basic"
+                label="Edit todo"
+                variant="standard"
+                placeholder={text}
+                onChange={handleInput}
+              />
+              <Button variant="text" onClick={editTask}>
+                Save Todo
+              </Button>
+            </label>
+          </Typography>
+        </Popover>
+      </li>
+    );
+  }
+  return null;
+};
+export default TodoItem;
--- src/store/todoSlice.js
+++ src/store/todoSlice.js
@@ -1,31 +1,36 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const todoSlice = createSlice({
-    name: 'todos',
-    initialState: {
-        todos:JSON.parse(localStorage.getItem('todos'))||[]
+  name: 'todos',
+  initialState: {
+    todos: JSON.parse(localStorage.getItem('todos')) || [],
+  },
+  reducers: {
+    addTodo(state, action) {
+      state.todos.push({
+        userid: sessionStorage.getItem('currentloggedin'),
+        id: new Date().toISOString(),
+        text: action.payload.text,
+        completed: false,
+      });
+      localStorage.setItem('todos', JSON.stringify([...state.todos]));
     },
-    reducers: {
-        addTodo(state, action) {
-            state.todos.push({
-                userid:sessionStorage.getItem('currentloggedin'),
-                id:new Date().toISOString(),
-                text: action.payload.text,
-                completed:false,
-              });
-              localStorage.setItem('todos',JSON.stringify([...state.todos]))
-        },
-        removeTodo(state, action) {
-            state.todos = state.todos.filter((todo) => todo.id!==action.payload.id)
-            localStorage.setItem('todos', JSON.stringify([...state.todos]));
-        },
-        toggleTodoComplete(state, action) {
-            const toggleTodo = state.todos.find((todo) => todo.id === action.payload.id);
-            toggleTodo.completed = !toggleTodo.completed;
-            localStorage.setItem('todos', JSON.stringify([...state.todos]))
-        }
+    removeTodo(state, action) {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload.id);
+      localStorage.setItem('todos', JSON.stringify([...state.todos]));
     },
-})
+    toggleTodoComplete(state, action) {
+      const toggleTodo = state.todos.find((todo) => todo.id === action.payload.id);
+      toggleTodo.completed = !toggleTodo.completed;
+      localStorage.setItem('todos', JSON.stringify([...state.todos]));
+    },
+    editTodo(state, action) {
+      const editTodoItem = state.todos.find((todo) => todo.id === action.payload.id);
+      editTodoItem.text = action.payload.text;
+      localStorage.setItem('todos', JSON.stringify([...state.todos]));
+    },
+  },
+});
 
-export const {addTodo, removeTodo, toggleTodoComplete} = todoSlice.actions;
+export const { addTodo, removeTodo, toggleTodoComplete, editTodo } = todoSlice.actions;
 export default todoSlice.reducer;
